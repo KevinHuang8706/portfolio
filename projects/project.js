bb67509ac94d13bf59d8987d67025e3fbffce0a8
KevinHuang8706 <kevinhty03@gmail.com>
2025-04-29 21:08:47 -0700

    fixed project count

--- projects/project.js
+++ projects/project.js
@@ -4,8 +4,8 @@ import { fetchJSON, renderProjects } from '../global.js';
     const projects = await fetchJSON('../lib/projects.json');
     const projectsContainer = document.querySelector('.projects');
     renderProjects(projects, projectsContainer);
-    const titleElement = document.querySelector('.projects-title');
-    if (titleElement) titleElement.innerHTML += ` (${projects.length})`;
+    const titleElement = document.querySelector('h1');
+    if (titleElement) titleElement.innerHTML += `: ${projects.length} Projects`;
 })();
 
 
